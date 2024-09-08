dd9b76f24d6d0ab68d7caa272c1b5a2987b2daf3
rakesh <rakeshbaddipadaga8@gmail.com>
2024-09-09 01:07:08 +0530

    changes

--- frontend/src/apis/api.js
+++ frontend/src/apis/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_URL = 'https://taskmanger-psi.vercel.app/api';
 
 export const register = (data) => axios.post(`${API_URL}/register`, data);
-export const login = (data) => axios.post(`${API_URL}/`, data);
+export const login = (data) => axios.post(`${API_URL}/login`, data);
 export const fetchTasks = (token) => axios.get(`${API_URL}/tasks`, { headers: { Authorization: token } });
 export const createTask = (data, token) => axios.post(`${API_URL}/tasks`, data, { headers: { Authorization: token } });
 export const updateTask = (id, data, token) => axios.put(`${API_URL}/tasks/${id}`, data, { headers: { Authorization: token } });
